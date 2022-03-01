0236ad67b5fff26ea604d472c2cc89ea548f1177
goldipl <mgodlewski92@gmail.com>
2022-03-01 18:07:03 +0100

    1st commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// const sum = require("./sum");
+// import { sum2 } from "./sum2";
+import style from "./css/index.scss";
+// import Icon from './assets/img/proba.png';
+// console.log("Hello World");
+// console.log(sum.sum(2,2));
+// console.log(sum2(2,2));
+
+// let heading = document.querySelector("#demo"),
+//     sumValue = sum.sum(10,5);
+
+// heading.innerHTML = `2 + 15 = ${sumValue}`;
+
+// let myIcon = new Image();
+// myIcon.src = Icon;
+// document.querySelector("div").appendChild(myIcon);
